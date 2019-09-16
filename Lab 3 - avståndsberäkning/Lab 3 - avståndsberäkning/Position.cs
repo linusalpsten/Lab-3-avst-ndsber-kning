82ca7a7de94c7f4aa5875ef711fc9ac38d574ce4
Linus Alpsten <linus.alpsten@telia.com>
2019-09-16 13:31:19 +0200

    add position class

---
+++ Lab 3 - avståndsberäkning/Lab 3 - avståndsberäkning/Position.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3___avståndsberäkning
+{
+    class Position
+    {
+        public double latitude;
+        public double longitude;
+    }
+}
